24759c37d638e8e97b709cd17fb082556ca67236
Edward Ellis <edwardellis.egre@gmail.com>
2023-03-16 19:54:07 +0000

    Added java logging.

--- tictactoe-web/src/main/java/net/ellise/tictactoe/web/handler/IndexHandler.java
+++ tictactoe-web/src/main/java/net/ellise/tictactoe/web/handler/IndexHandler.java
@@ -27,21 +27,26 @@ public class IndexHandler implements HttpHandler {
         String cookieUsername = HandlerUtil.getCookie(exchange, COOKIE_USERNAME_KEY);
         String cookiePassword = HandlerUtil.getCookie(exchange, COOKIE_PASSWORD_KEY);
         if (userRepository.loadUser(cookieUsername, cookiePassword) != null) {
+            LOGGER.info(String.format("User %1$s logged in using cookies", cookieUsername));
             String htmlTemplate = HandlerUtil.loadPageFromClasspath("html/lobby.html");
             html = String.format(htmlTemplate, cookieUsername);
             responseCode = 200;
         } else if ("GET".equals(exchange.getRequestMethod())) {
+            LOGGER.info("GET request returning login page.");
             html = HandlerUtil.loadPageFromClasspath("html/index.html");
             responseCode = 200;
         } else if ("POST".equals(exchange.getRequestMethod())) {
+            LOGGER.info("POST request, processing log in attempt...");
             Map<String,String> attributes = HandlerUtil.parsePostAttributes(exchange);
             if (attributes.containsKey(USERNAME_KEY) && attributes.containsKey(PASSWORD_KEY)) {
                 String username = attributes.get(USERNAME_KEY);
                 String password = attributes.get(PASSWORD_KEY);
                 if (userRepository.loadUser(username, password) == null) {
+                    LOGGER.info(String.format("Log in for %1$s successful.", username));
                     html = HandlerUtil.loadPageFromClasspath("html/index_failed_login.html");
                     responseCode = 200;
                 } else {
+                    LOGGER.info(String.format("Log in for %1$s unsuccessful.", username));
                     HandlerUtil.writeCookie(exchange, COOKIE_USERNAME_KEY, username);
                     HandlerUtil.writeCookie(exchange, COOKIE_PASSWORD_KEY, password);
                     String htmlTemplate = HandlerUtil.loadPageFromClasspath("html/lobby.html");
--- tictactoe-web/src/main/java/net/ellise/tictactoe/web/handler/RegisterHandler.java
+++ tictactoe-web/src/main/java/net/ellise/tictactoe/web/handler/RegisterHandler.java
@@ -6,8 +6,10 @@ import net.ellise.tictactoe.repository.UserRepository;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class RegisterHandler implements HttpHandler {
+    private static final Logger LOGGER = Logger.getLogger(RegisterHandler.class.getCanonicalName());
     private static final String USERNAME_KEY = "username";
     private static final String PASSWORD_KEY = "password";
     private static final String CONFIRM_KEY = "confirm";
@@ -23,10 +25,12 @@ public class RegisterHandler implements HttpHandler {
         int responseCode = 503;
         switch(exchange.getRequestMethod()) {
             case "GET":
+                LOGGER.info("GET /register");
                 html = HandlerUtil.loadPageFromClasspath("html/register.html");
                 responseCode = 200;
                 break;
             case "POST":
+                LOGGER.info("POST /register");
                 Map<String,String> attributes = HandlerUtil.parsePostAttributes(exchange);
                 if (attributes.containsKey(USERNAME_KEY) &&
                         attributes.containsKey(PASSWORD_KEY) &&
@@ -39,13 +43,16 @@ public class RegisterHandler implements HttpHandler {
                         confirm != null && !confirm.isBlank() &&
                         confirm.equals(password)) {
                         if (userRepository.saveUser(username, password)) {
+                            LOGGER.info("User registration successful.");
                             html = HandlerUtil.loadPageFromClasspath("html/index.html");
                             responseCode = 200;
                         } else {
+                            LOGGER.info("User registration failed, perhaps username already exists.");
                             html = HandlerUtil.loadPageFromClasspath("html/register.html");
                             responseCode = 200;
                         }
                     } else {
+                        LOGGER.info("Invalid User registration, username blank, password blank or password not confirmed.");
                         html = HandlerUtil.loadPageFromClasspath("html/register.html");
                         responseCode = 200;
                     }
